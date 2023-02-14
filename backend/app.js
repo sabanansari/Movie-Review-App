765f68d51f95dcfbf8e97be9f94af3b5e99b04aa
sabanansari <alisaban52@gmail.com>
2023-02-14 23:48:36 +0530

    added post api to create new User and save into local MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+require("./db");
 const userRouter = require('./routes/user');
 const app = express();
 
-app.use(userRouter);
+app.use(express.json());
+app.use("/api/user",userRouter);
 
 app.listen(3000, ()=>{
     console.log("The server is successfully connected to port 3000");
---
+++ backend/db/index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1:27017/review_app').
+then(()=>{
+    console.log('db is connected!');
+}).catch((ex)=>{
+    console.log("db connection failed:",ex);
+});
