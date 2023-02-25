136f86e23dd3ef91963d05418ce7cf52d5cbde2d
sabanansari <alisaban52@gmail.com>
2023-02-26 01:51:33 +0530

    created sign up and forget password form

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -5,19 +5,20 @@ import FormInput from '../form/FormInput';
 import Submit from '../form/Submit';
 import CustomLink from '../CustomLink';
 
-export default function Signin() {
+export default function Signup() {
   return (
     <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
         <Container>
             <form  className='bg-secondary rounded p-6 w-72 space-y-6'>
-                <Title>Sign In</Title>
+                <Title>Sign Up</Title>
+                <FormInput label='Name' placeholder='Example' name = 'name'></FormInput>
                 <FormInput label='Email' placeholder='example@email.com' name = 'email'></FormInput>
                 <FormInput label='Password' placeholder='********' name = 'password'></FormInput>
-                <Submit value="Sign In"></Submit>
+                <Submit value="Sign Up"></Submit>
 
                 <div className="flex justify-between">
                     <CustomLink to='/auth/forget-password'>Forget Password</CustomLink>
-                    <CustomLink to='/auth/signup'>Sign Up</CustomLink>
+                    <CustomLink to='/auth/signin'>Sign In</CustomLink>
                 </div>
             </form>
         </Container>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App/>);
+root.render(
+<BrowserRouter>
+    <App/>
+</BrowserRouter>
+
+);
 
